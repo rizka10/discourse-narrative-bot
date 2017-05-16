3bde867f2580e820ce65df3db06b04e293dac05e
Guo Xiang Tan <tgx_world@hotmail.com>
2017-05-16 16:30:24 +0800

    FIX: Welcome message should not ask user to reply to topic.

--- jobs/send_default_welcome_message.rb
+++ jobs/send_default_welcome_message.rb
@@ -6,7 +6,7 @@ module Jobs
         params = SystemMessage.new(user).defaults
 
         title = I18n.t("system_messages.#{type}.subject_template", params)
-        raw = I18n.t("system_messages.#{type}.text_body_template", params)
+        raw = I18n.t("discourse_narrative_bot.#{type}.text_body_template", params)
 
         PostCreator.create!(
           User.find(-2),
